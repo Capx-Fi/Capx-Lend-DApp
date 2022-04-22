680a759ddd157feada41e8e075cf08c6b0a157c8
Sooraj M Singh <soorajsingh1999@gmail.com>
2022-04-22 13:52:07 +0530

    styled sidebar

--- src/components/layout/SideBar/index.js
+++ src/components/layout/SideBar/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
-import { SvgIcon } from "../../common";
+import DashboardIcon  from "../../../assets/images/svg/Dashboard.svg";
+import LiquidIcon from "../../../assets/images/svg/Liquid.svg";
+import WalletIcon from "../../../assets/images/svg/Wallet.svg";
+import ArrowIcon from "../../../assets/images/svg/arrow-up-right.svg";
 import "./index.less";
 
 const SideBar = () => {
@@ -9,14 +11,14 @@ const SideBar = () => {
         <div className="sidebar-inner">
             <div className="sidebar-menu">
                 <ul>
-                    <li><NavLink to="/lend-borrow" className={(navData) => (navData.isActive ? "selected" : '')}><SvgIcon name="rect" viewbox="0 0 24 24" /> Lend/Borrow</NavLink></li>
-                    <li><NavLink to="/" className={(navData) => (navData.isActive ? "selected" : '')}><SvgIcon name="rect" viewbox="0 0 24 24" /> Dashboard</NavLink></li>
-                    <li><NavLink to="/Liquidation" className={(navData) => (navData.isActive ? "selected" : '')}><SvgIcon name="rect" viewbox="0 0 24 24" /> Liquidation</NavLink></li>
+                    <li><NavLink to="/lend-borrow" className={(navData) => (navData.isActive ? "selected" : '')}><img src={WalletIcon} className="sidebar-icon" alt="icon" /> Lend/Borrow</NavLink></li>
+                    <li><NavLink to="/" className={(navData) => (navData.isActive ? "selected" : '')}><img src={DashboardIcon} alt="icon" className="sidebar-icon" /> Dashboard</NavLink></li>
+                    <li><NavLink to="/Liquidation" className={(navData) => (navData.isActive ? "selected" : '')}><img src={LiquidIcon} alt="icon" className="sidebar-icon"  /> Liquidation</NavLink></li>
                 </ul>
             </div>
             <div className="sidebar-bottom">
-                <Link to="/">Capx Liquid <SvgIcon name="top-right" viewbox="0 0 12.083 12.083" /></Link>
-                <Link to="/">Capx Exchange <SvgIcon name="top-right" viewbox="0 0 12.083 12.083" /></Link>
+                <a href="https://liquid.capx.fi" target="_blank" rel="noreferrer" >Capx Liquid<img src={ArrowIcon} alt="icon" className="sidebar-icon" /></a>
+                <a href="https://exchange.capx.fi" target="_blank" rel="noreferrer">Capx Exchange<img src={ArrowIcon} alt="icon" className="sidebar-icon" /></a>
             </div>
         </div>
     )
