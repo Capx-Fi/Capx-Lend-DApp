008cb818edb4bc9c1e3c285991a7430a716b0a0e
Sooraj M Singh <soorajsingh1999@gmail.com>
2022-04-25 15:02:19 +0530

    added metamask functionality

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import SideBar from "./components/layout/SideBar";
 import NavigationBar from "./components/layout/NavigationBar";
 import Dashboard from "./containers/Dashboard";
 import LendBorrow from "./containers/Lend-Borrow";
+import Metamask from "./containers/Metamask";
 
 //Svg Sprite
 import svgFile from "./assets/images/svg/svg-sprite.svg";
 
+// Metamask imports
+import { InjectedConnector } from "wagmi/connectors/injected";
+
 import "./App.less";
+import { chain, useConnect } from "wagmi";
 
 const { Header, Content, Sider, Footer } = Layout;
 
@@ -25,27 +30,52 @@ const Routes = () => {
 };
 
 const App = () => {
-	return (
-		<>
-			<SvgSprite url={svgFile} />
-			<Layout>
-				<Header className="header">
-					<NavigationBar />
-				</Header>
-				<Layout className="main-content">
-					<Sider width={290} className="site-layout-background">
-						<SideBar />
-					</Sider>
-					<Content className="right-content-wrapper">
-						<Routes />
-					</Content>
+	// metamask configuration
+	const isConnected = useConnect();
+	// console.log(isConnected);
+	console.log(isConnected.isConnected);
+	if (isConnected?.isConnected) {
+		return (
+			<>
+				<SvgSprite url={svgFile} />
+				<Layout>
+					<Header className="header">
+						<NavigationBar />
+					</Header>
+					<Layout className="main-content">
+						<Sider width={290} className="site-layout-background">
+							<SideBar />
+						</Sider>
+						<Content className="right-content-wrapper">
+							<Routes />
+						</Content>
+					</Layout>
+					<Footer className="main-footer">
+						© 2021 Capx All rights reserved.
+					</Footer>
 				</Layout>
-				<Footer className="main-footer">
-					© 2021 Capx All rights reserved.
-				</Footer>
-			</Layout>
-		</>
-	);
+			</>
+		);
+	} else {
+		return (
+			<>
+				<SvgSprite url={svgFile} />
+				<Layout>
+					<Header className="header">
+						<NavigationBar />
+					</Header>
+					<Layout className="main-content">
+						<Content className="right-content-wrapper">
+							<Metamask />
+						</Content>
+					</Layout>
+					<Footer className="main-footer">
+						© 2021 Capx All rights reserved.
+					</Footer>
+				</Layout>
+			</>
+		);
+	}
 };
 
 export default App;
--- src/components/layout/NavigationBar/index.js
+++ src/components/layout/NavigationBar/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Menu, Dropdown } from "antd";
 import { SvgIcon } from "../../common";
+import { useAccount, useDisconnect, useConnect } from "wagmi";
 import "./index.less";
 
 const NavigationBar = () => {
@@ -9,11 +10,11 @@ const NavigationBar = () => {
 			selectable
 			items={[
 				{
-					label: <a href='https://www.antgroup.com'>1st menu item</a>,
+					label: <a href="https://www.antgroup.com">1st menu item</a>,
 					key: "0",
 				},
 				{
-					label: <a href='https://www.aliyun.com'>2nd menu item</a>,
+					label: <a href="https://www.aliyun.com">2nd menu item</a>,
 					key: "1",
 				},
 				{
@@ -26,38 +27,48 @@ const NavigationBar = () => {
 			]}
 		/>
 	);
-	const address = "0xA9B6401865F788e7ca031f174F67c29d74865395";
 	const currentChain = "Ethereum";
-
+	const { data: account } = useAccount();
+	console.log(account?.address);
+	const address = account?.address;
+	const { disconnect } = useDisconnect();
+	const isConnected = useConnect().isConnected;
 	return (
 		<>
-			<SvgIcon name='lend-logo' className='logo' viewbox='0 0 2130 552' />
-			<div className='top-bar-right'>
-				<div className='notification'>
-					<Dropdown overlay={menu}>
-						<SvgIcon
-							name='bell'
-							className='notification-bell'
-							viewbox='0 0 24 24'
-						/>
-					</Dropdown>
-				</div>
-				<Dropdown overlay={menu} trigger={["click"]}>
-					<button
-						className='ant-dropdown-link'
-						onClick={(e) => e.preventDefault()}
-					>
-						{currentChain}
-						<SvgIcon name='dropdown-arrow' viewbox='0 0 24 24' fill='none' />
-					</button>
-				</Dropdown>
-				<div className='address' onClick={console.log("disconnect?")}>
-					<div className='address-right'>
-						{address.slice(0, 6)}...{address.slice(-4)}
+			<SvgIcon name="lend-logo" className="logo" viewbox="0 0 2130 552" />
+			{isConnected && (
+				<div className="top-bar-right">
+					<div className="notification">
+						<Dropdown overlay={menu}>
+							<SvgIcon
+								name="bell"
+								className="notification-bell"
+								viewbox="0 0 24 24"
+							/>
+						</Dropdown>
 					</div>
-					<SvgIcon name='dropdown-arrow' viewbox='0 0 24 24' fill='none' />
+					<Dropdown overlay={menu} trigger={["click"]}>
+						<button
+							className="ant-dropdown-link"
+							onClick={(e) => e.preventDefault()}
+						>
+							{currentChain}
+							<SvgIcon name="dropdown-arrow" viewbox="0 0 24 24" fill="none" />
+						</button>
+					</Dropdown>
+					<div className="address" onClick={disconnect}>
+						<div className="address-right">
+							{address?.slice(0, 6)}...{address?.slice(-4)}
+						</div>
+						<SvgIcon
+							name="logout"
+							viewbox="0 0 24 24"
+							fill="#7fe7a3"
+							onClick={disconnect}
+						/>
+					</div>
 				</div>
-			</div>
+			)}
 		</>
 	);
 };
