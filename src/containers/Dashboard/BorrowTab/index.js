da0b5f2bc9e72249660b169e0958122a3bde6b72
Shreyas Agarwal <adityarola@capx.fi>
2022-04-30 18:07:50 +0530

    Borrow Dashboard | Filters Done

--- src/containers/Dashboard/BorrowTab/index.js
+++ src/containers/Dashboard/BorrowTab/index.js
@@ -14,10 +14,11 @@ import { getAdditionalInfo } from "../../../utils/getAdditionalInfo";
 import Web3 from "web3";
 import { MASTER_ABI } from "../../../contracts/Master";
 import { ORACLE_ABI } from "../../../contracts/Oracle";
-import { fetchBorrowerLoans } from "../../../utils/fetchBorrowerLoans";
+import { fetchLoanDetails } from "../../../utils/fetchLoanDetails";
 import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
 import { convertToInternationalCurrencySystem } from "../../../utils/convertToInternationalCurrencySystem";
-
+import { getFilterValues } from "../../../utils/getFilterValues";
+import { fetchBorrowerLoans } from "../../../utils/fetchBorrowerLoans";
 const { Option } = Select;
 
 const BorrowTab = () => {
@@ -48,11 +49,13 @@ const BorrowTab = () => {
 
   const getLoans = async() => {
     const _loans = await fetchBorrowerLoans(
+    "0xBC7a2925D5C194D1DbEdeB99F13c326851dC8230",
     "https://api.thegraph.com/subgraphs/name/shreyas3336/capx-lend",
     masterContract,
     oracleContract
     );
     console.log("L",_loans);
+    console.log("Filters", getFilterValues(_loans,"stableCoinTicker"));
     return _loans;
 }
 
@@ -89,6 +92,18 @@ const BorrowTab = () => {
     return status;
   }
 
+  function filterLoansByCompanyAsset(loans, companyAsset) {
+    if (companyAsset !== "") {
+      setFilteredLoans(loans.filter((loan) => loan.collateralTicker === companyAsset));
+    } else setFilteredLoans(loans);
+  }
+
+  function filterLoansByLendAsset(loans, lendAsset) {
+    if (lendAsset !== "") {
+      setFilteredLoans(loans.filter((loan) => loan.stableCoinTicker === lendAsset));
+    } else setFilteredLoans(loans);
+  }
+
   function filterLoansByStatus(loans, status) {
     if (status !== "")
       setFilteredLoans(loans.filter((loan) => loan.status === status));
@@ -103,7 +118,7 @@ const BorrowTab = () => {
             <ul>
               <li>
                 <p>Borrowed Amount</p>
-                <h4>{convertToInternationalCurrencySystem(totalAmount(loans))} </h4>
+                <h4>$ {convertToInternationalCurrencySystem(totalAmount(loans))} </h4>
               </li>
               <li>
                 <p>Active loans</p>
@@ -111,11 +126,11 @@ const BorrowTab = () => {
               </li>
               <li>
                 <p>Interest Paid</p>
-                <h4>${convertToInternationalCurrencySystem(totalInterest(loans))}</h4>
+                <h4>$ {convertToInternationalCurrencySystem(totalInterest(loans))}</h4>
               </li>
               <li>
                 <p>Loan Amount Repayed</p>
-                <h4>${convertToInternationalCurrencySystem(totalPaidOff(loans))}</h4>
+                <h4>$ {convertToInternationalCurrencySystem(totalPaidOff(loans))}</h4>
               </li>
             </ul>
           </div>
@@ -129,26 +144,32 @@ const BorrowTab = () => {
             placeholder="Loan Type"
             bordered={false}
           >
-            <Option value="1">Loan Type</Option>
-            <Option value="2">Loan Type</Option>
+            <Option value="Single Repayment">Single Repayment</Option>
+            {/* <Option value="Instalment Repayment">Instalment Repayment</Option> */}
           </Select>
           <Select
             dropdownClassName="capx-dropdown"
             suffixIcon={<SvgIcon name="arrow-down" viewbox="0 0 18 10.5" />}
             placeholder="Company Asset"
             bordered={false}
+            onChange={(e) => filterLoansByCompanyAsset(loans, e)}
           >
-            <Option value="1">Company Asset</Option>
-            <Option value="2">Company Asset</Option>
+            <Option value={""}>All</Option>
+            {getFilterValues(loans, "collateralTicker").map(function (wvt_asset) {
+              return <Option value={wvt_asset}>{wvt_asset}</Option>;
+            })}
           </Select>
           <Select
             dropdownClassName="capx-dropdown"
             suffixIcon={<SvgIcon name="arrow-down" viewbox="0 0 18 10.5" />}
             placeholder="Lending Asset"
             bordered={false}
+            onChange={(e) => filterLoansByLendAsset(loans, e)}
           >
-            <Option value="1">Lending Asset</Option>
-            <Option value="2">Lending Asset</Option>
+            <Option value={""}>All</Option>
+            {getFilterValues(loans, "stableCoinTicker").map(function (wvt_asset) {
+              return <Option value={wvt_asset}>{wvt_asset}</Option>;
+            })}
           </Select>
           <Select
             dropdownClassName="capx-dropdown"
@@ -170,9 +191,9 @@ const BorrowTab = () => {
             placeholder="Sort By"
             style={{ minWidth: 120 }}
           >
-            <Option value="sb1">Order Status</Option>
-            <Option value="sb2">Order Type</Option>
-            <Option value="sb3">Health Factor</Option>
+            <Option value="sb1">Loan Amount</Option>
+            <Option value="sb2">Interest Rate</Option>
+            <Option value="sb3">Loan-To-Value</Option>
           </Select>
         </Col>
         <Col sm="12">
