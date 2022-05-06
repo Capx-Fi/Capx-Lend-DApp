ee834c9bb27de4d458229cd224929c8fb9aff407
Sooraj M Singh <soorajsingh1999@gmail.com>
2022-05-06 20:47:14 +0530

    added no orders found illustration v1

--- src/containers/Lend-Borrow/ViewProjects/LendTabLB/index.js
+++ src/containers/Lend-Borrow/ViewProjects/LendTabLB/index.js
@@ -1,10 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Button, Select } from "antd";
-import {
-  SvgIcon,
-  Row,
-  Col,
-} from "../../../../components/common";
+import { SvgIcon, Row, Col } from "../../../../components/common";
 import "./index.less";
 import MarketLoader from "../../../../components/common/market-loader/MarketLoader";
 import { LEND_ABI } from "../../../../contracts/Lend";
@@ -18,196 +14,208 @@ import { fetchLoanDetailsBorrower } from "../../../../utils/fetchLoanDetailsBorr
 import NewLendOfferComponent from "../../NewLendOfferComponent";
 import { useWeb3React, UnsupportedChainIdError } from "@web3-react/core";
 import { getFilterValues } from "../../../../utils/getFilterValues";
+import noBorrow from "../../../../assets/images/noBorrow.png";
 
 const { Option } = Select;
 
 const LendTabLB = (collapsed) => {
-  const [loans, setLoans] = useState(null);
-  const [filteredLoans, setFilteredLoans] = useState(null);
-  const web3 = new Web3(Web3.givenProvider);
-  const masterContract = new web3.eth.Contract(
-    MASTER_ABI,
-    "0x793130DFbFDC30629015C0f07b41Dc97ec14d8B5"
-  );
-  const oracleContract = new web3.eth.Contract(
-    ORACLE_ABI,
-    "0x49d396Eb1B3E2198C32D2FE2C7146FD64f8BcF27"
-  );
-  const lendContract = new web3.eth.Contract(
-    LEND_ABI,
-    "0x309D0Ff4b655bAD183A3FA88A0547b41e877DcF1"
-  );
-  const { active, account } = useWeb3React();
-  useEffect(() => {
-    active &&
-      getLoans().then((loans) => {
-        setFilteredLoans(loans);
-        setLoans(loans);
-      });
-  }, []);
+	const [loans, setLoans] = useState(null);
+	const [filteredLoans, setFilteredLoans] = useState(null);
+	const web3 = new Web3(Web3.givenProvider);
+	const masterContract = new web3.eth.Contract(
+		MASTER_ABI,
+		"0x793130DFbFDC30629015C0f07b41Dc97ec14d8B5"
+	);
+	const oracleContract = new web3.eth.Contract(
+		ORACLE_ABI,
+		"0x49d396Eb1B3E2198C32D2FE2C7146FD64f8BcF27"
+	);
+	const lendContract = new web3.eth.Contract(
+		LEND_ABI,
+		"0x309D0Ff4b655bAD183A3FA88A0547b41e877DcF1"
+	);
+	const { active, account } = useWeb3React();
+	useEffect(() => {
+		active &&
+			getLoans().then((loans) => {
+				setFilteredLoans(loans);
+				setLoans(loans);
+			});
+	}, []);
 
-  //filter logic
-  const [filters, setFilters] = useState({
-    lendAsset: "",
-    companyAsset: "",
-    status: "",
-  });
+	//filter logic
+	const [filters, setFilters] = useState({
+		lendAsset: "",
+		companyAsset: "",
+		status: "",
+	});
 
-  useEffect(() => {
-    let finalLoans = loans;
-    if (filters.lendAsset !== "") {
-      finalLoans = finalLoans.filter(
-        (loan) => loan.stableCoinTicker === filters.lendAsset
-      );
-    }
-    if (filters.companyAsset !== "") {
-      finalLoans = finalLoans.filter(
-        (loan) => loan.collateralTicker === filters.companyAsset
-      );
-    }
-    if (filters.status !== "") {
-      finalLoans = finalLoans.filter((loan) => loan.status === filters.status);
-    }
+	useEffect(() => {
+		let finalLoans = loans;
+		if (filters.lendAsset !== "") {
+			finalLoans = finalLoans.filter(
+				(loan) => loan.stableCoinTicker === filters.lendAsset
+			);
+		}
+		if (filters.companyAsset !== "") {
+			finalLoans = finalLoans.filter(
+				(loan) => loan.collateralTicker === filters.companyAsset
+			);
+		}
+		if (filters.status !== "") {
+			finalLoans = finalLoans.filter((loan) => loan.status === filters.status);
+		}
 
-    setFilteredLoans(finalLoans);
-  }, [filters, loans]);
+		setFilteredLoans(finalLoans);
+	}, [filters, loans]);
 
-  function filterLoansByCompanyAsset(companyAsset) {
-    setFilters({ ...filters, companyAsset });
-  }
+	function filterLoansByCompanyAsset(companyAsset) {
+		setFilters({ ...filters, companyAsset });
+	}
 
-  function filterLoansByLendAsset(lendAsset) {
-    setFilters({ ...filters, lendAsset });
-  }
+	function filterLoansByLendAsset(lendAsset) {
+		setFilters({ ...filters, lendAsset });
+	}
 
-  //end of filter logic
+	//end of filter logic
 
-  const getLoans = async () => {
-    const _loans = await fetchLoanDetailsBorrower(
-      account,
-      "https://api.thegraph.com/subgraphs/name/shreyas3336/capx-lend",
-      masterContract,
-      oracleContract
-    );
-    console.log("L", _loans);
-    console.log("Filters", getFilterValues(_loans, "stableCoinTicker"));
-    console.log("LOANS", _loans);
-    return _loans;
-  };
+	const getLoans = async () => {
+		const _loans = await fetchLoanDetailsBorrower(
+			account,
+			"https://api.thegraph.com/subgraphs/name/shreyas3336/capx-lend",
+			masterContract,
+			oracleContract
+		);
+		console.log("L", _loans);
+		console.log("Filters", getFilterValues(_loans, "stableCoinTicker"));
+		console.log("LOANS", _loans);
+		return _loans;
+	};
 
-  function availableLoanStatus(loans) {
-    let status = [];
-    loans.forEach((loan) => {
-      if (!status.includes(loan.status)) status.push(loan.status);
-    });
+	function availableLoanStatus(loans) {
+		let status = [];
+		loans.forEach((loan) => {
+			if (!status.includes(loan.status)) status.push(loan.status);
+		});
 
-    return status;
-  }
+		return status;
+	}
 
-  const pathname = window.location.pathname;
-  return !pathname.includes("/new") ? (
-    filteredLoans ? (
-      <>
-      <h1 className="mb-2">Marketplace</h1>
-        <Row className="heading-row">
-          <Col className="left-col">
-            <h3> Filter By </h3>
-            <div className="filter-container">
-              <div className="select-container">
-                <p>{"Company Asset:"}</p>
-                <Select
-                  dropdownClassName="capx-dropdown"
-                  suffixIcon={
-                    <SvgIcon name="arrow-down" viewbox="0 0 18 10.5" />
-                  }
-                  defaultValue=""
-                  bordered={false}
-                  onChange={(e) => filterLoansByCompanyAsset(e)}
-                >
-                  <Option value={""}>All</Option>
-                  {getFilterValues(filteredLoans, "collateralTicker").map(
-                    function (wvt_asset) {
-                      return <Option value={wvt_asset}>{wvt_asset}</Option>;
-                    }
-                  )}
-                </Select>
-              </div>
-              <div className="select-container">
-                <p>{"Lending Asset:"}</p>
-                <Select
-                  dropdownClassName="capx-dropdown"
-                  suffixIcon={
-                    <SvgIcon name="arrow-down" viewbox="0 0 18 10.5" />
-                  }
-                  defaultValue=""
-                  bordered={false}
-                  onChange={(e) => filterLoansByLendAsset(e)}
-                >
-                  <Option value={""}>All</Option>
-                  {getFilterValues(filteredLoans, "stableCoinTicker").map(
-                    function (wvt_asset) {
-                      return <Option value={wvt_asset}>{wvt_asset}</Option>;
-                    }
-                  )}
-                </Select>
-              </div>
-            </div>
-          </Col>
+	const pathname = window.location.pathname;
+	return !pathname.includes("/new") ? (
+		filteredLoans ? (
+			<>
+				<h1 className="mb-2">Marketplace</h1>
+				<Row className="heading-row">
+					<Col className="left-col">
+						<h3> Filter By </h3>
+						<div className="filter-container">
+							<div className="select-container">
+								<p>{"Company Asset:"}</p>
+								<Select
+									dropdownClassName="capx-dropdown"
+									suffixIcon={
+										<SvgIcon name="arrow-down" viewbox="0 0 18 10.5" />
+									}
+									defaultValue=""
+									bordered={false}
+									onChange={(e) => filterLoansByCompanyAsset(e)}
+								>
+									<Option value={""}>All</Option>
+									{getFilterValues(filteredLoans, "collateralTicker").map(
+										function (wvt_asset) {
+											return <Option value={wvt_asset}>{wvt_asset}</Option>;
+										}
+									)}
+								</Select>
+							</div>
+							<div className="select-container">
+								<p>{"Lending Asset:"}</p>
+								<Select
+									dropdownClassName="capx-dropdown"
+									suffixIcon={
+										<SvgIcon name="arrow-down" viewbox="0 0 18 10.5" />
+									}
+									defaultValue=""
+									bordered={false}
+									onChange={(e) => filterLoansByLendAsset(e)}
+								>
+									<Option value={""}>All</Option>
+									{getFilterValues(filteredLoans, "stableCoinTicker").map(
+										function (wvt_asset) {
+											return <Option value={wvt_asset}>{wvt_asset}</Option>;
+										}
+									)}
+								</Select>
+							</div>
+						</div>
+					</Col>
 
-          <Col className="right-col">
-            <Select
-              dropdownClassName="capx-dropdown"
-              suffixIcon={<SvgIcon name="arrow-down" viewbox="0 0 18 10.5" />}
-              placeholder="Sort By"
-              style={{ minWidth: 120 }}
-            >
-              <Option value="sb1">Loan Amount</Option>
-              <Option value="sb2">Interest Rate</Option>
-              <Option value="sb3">Loan-To-Value</Option>
-            </Select>
-          </Col>
-        </Row>
-        <Row>
-          <Col sm="12">
-            <h2>All Offers</h2>
-          </Col>
-          <Col>
-            <div className="order-list">
-              {availableLoanStatus(filteredLoans).map(function (status) {
-                return (
-                  <div className="orderlist-card">
-                    {filteredLoans.map(function (loan) {
-                      return (
-                        loan.status === status && (
-                          <AccordionCard
-                            orderId={loan.loanID}
-                            healthFactor={loan.healthFactor}
-                            paymentType={loan.repaymentType}
-                            //   status={loan.status}
-                            orderDetails={getOrderDetails(loan)}
-                            additonalInfo={getAdditionalInfo(loan)}
-                            loan={loan}
-                            isBorrower={false}
-                            lendContract={lendContract}
-                            masterContract={masterContract}
-                            externalLiquidation={true} // needs to be updated from the UI
-                          />
-                        )
-                      );
-                    })}
-                  </div>
-                );
-              })}
-            </div>
-          </Col>
-        </Row>
-      </>
-    ) : (
-      <MarketLoader />
-    )
-  ) : (
-    <NewLendOfferComponent lend_loan_assets />
-  );
+					<Col className="right-col">
+						<Select
+							dropdownClassName="capx-dropdown"
+							suffixIcon={<SvgIcon name="arrow-down" viewbox="0 0 18 10.5" />}
+							placeholder="Sort By"
+							style={{ minWidth: 120 }}
+						>
+							<Option value="sb1">Loan Amount</Option>
+							<Option value="sb2">Interest Rate</Option>
+							<Option value="sb3">Loan-To-Value</Option>
+						</Select>
+					</Col>
+				</Row>
+				<Row>
+					<Col sm="12">
+						<h2>All Offers</h2>
+					</Col>
+					<Col>
+						<div className="order-list">
+							{availableLoanStatus(filteredLoans).map(function (status) {
+								return (
+									<div className="orderlist-card">
+										{filteredLoans.map(function (loan) {
+											return (
+												loan.status === status && (
+													<AccordionCard
+														orderId={loan.loanID}
+														healthFactor={loan.healthFactor}
+														paymentType={loan.repaymentType}
+														//   status={loan.status}
+														orderDetails={getOrderDetails(loan)}
+														additonalInfo={getAdditionalInfo(loan)}
+														loan={loan}
+														isBorrower={false}
+														lendContract={lendContract}
+														masterContract={masterContract}
+														externalLiquidation={true} // needs to be updated from the UI
+													/>
+												)
+											);
+										})}
+									</div>
+								);
+							})}
+							{availableLoanStatus(filteredLoans)?.length === 0 && (
+								<div className="no-orders">
+									{/* <SvgIcon name="no-borrow" viewbox="0 0 180 150" /> */}
+									<img
+										src={`${noBorrow}`}
+										alt="no borrow order illustration"
+										width={320}
+									/>
+									<h3>Oops! No Lend Orders Found!</h3>
+								</div>
+							)}
+						</div>
+					</Col>
+				</Row>
+			</>
+		) : (
+			<MarketLoader />
+		)
+	) : (
+		<NewLendOfferComponent lend_loan_assets />
+	);
 };
 
 export default LendTabLB;
