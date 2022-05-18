3911990cc605c504003dbe61f6676a1c397a7bd1
pulkit <pulkitm404@gmail.com>
2022-05-18 18:02:47 +0530

    added-breakpoint--initiated -eth-chainid-based-config

--- src/constants/getChainConfig.js
+++ src/constants/getChainConfig.js
@@ -10,34 +10,27 @@ import {
 	CONTRACT_ADDRESS_MASTER_ETHEREUM,
 	CONTRACT_ADDRESS_ORACLE_ETHEREUM,
 	CONTRACT_ADDRESS_LEND_ETHEREUM,
+	ETHEREUM_CHAIN_ID,
 } from "./config.js";
 
-// export const getExplorerURL = (chainId) => {
-// 	const explorerURL =
-// 		chainId?.toString() === BSC_CHAIN_ID.toString()
-// 			? EXPLORER_BSC
-// 			: chainId?.toString() === MATIC_CHAIN_ID.toString()
-// 			? EXPLORER_MATIC
-// 			: EXPLORER_ETHEREUM;
-// 	return explorerURL;
-// };
 
 export const getMasterURL = (chainId) => {
-	const masterURL = GRAPHAPIURL_MASTER_ETHEREUM;
+	const masterURL = chainId.toString() === ETHEREUM_CHAIN_ID.toString() ? GRAPHAPIURL_MASTER_ETHEREUM : GRAPHAPIURL_MASTER_ETHEREUM;
 	return masterURL;
 };
 
 export const getMasterContract = (chainId) => {
-	const masterContract = CONTRACT_ADDRESS_MASTER_ETHEREUM;
+	const masterContract = chainId.toString() === ETHEREUM_CHAIN_ID.toString() ? CONTRACT_ADDRESS_MASTER_ETHEREUM : CONTRACT_ADDRESS_MASTER_ETHEREUM;
+
 	return masterContract;
 };
 
 export const getOracleContract = (chainId) => {
-	const oracleContract = CONTRACT_ADDRESS_ORACLE_ETHEREUM;
+	const oracleContract = chainId.toString() === ETHEREUM_CHAIN_ID.toString() ? CONTRACT_ADDRESS_ORACLE_ETHEREUM : CONTRACT_ADDRESS_ORACLE_ETHEREUM;
 	return oracleContract;
 };
 
 export const getLendContract = (chainId) => {
-	const lendContract = CONTRACT_ADDRESS_LEND_ETHEREUM;
+	const lendContract = chainId.toString() === ETHEREUM_CHAIN_ID.toString() ? CONTRACT_ADDRESS_LEND_ETHEREUM : CONTRACT_ADDRESS_LEND_ETHEREUM;
 	return lendContract;
 };
